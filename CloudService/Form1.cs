aa7a5da892d16a9b2ab47951e7668ad7b55269e5
kiwi-juicee <kiranixis@gmail.com>
2021-01-22 19:42:14 +0300

    Сжатие выбранных файлов

--- CloudService/Form1.cs
+++ CloudService/Form1.cs
@@ -12,9 +12,18 @@ namespace CloudService
 {
     public partial class Form1 : Form
     {
+        Zipper zip = new Zipper();
         public Form1()
         {
             InitializeComponent();
+            openFileDialog1.ShowDialog();
+            IEnumerable<string> fileNames = openFileDialog1.FileNames;
+            zip.Add(fileNames);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ CloudService/Zipper.cs
@@ -0,0 +1,28 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloudService
+{
+    public class Zipper
+    {
+        public void Add(IEnumerable<string> filenames)
+        {
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.Password = "xxx";
+                string path = Path.GetDirectoryName(filenames.First());
+                foreach (var str in filenames)
+                    zip.AddFile(str).FileName = Path.GetFileName(str);
+                zip.Encryption = EncryptionAlgorithm.WinZipAes128;
+                zip.Save(path + "//archive.zip");
+                MessageBox.Show("File Zipped!");
+            }
+        }
+    }
+}
